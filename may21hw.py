d706b385778e9b749bcde6d62184b8a1cab4a463
om patil <omdeven1432@gmail.com>
2026-05-22 15:19:58 +0530

    Add files via upload

---
+++ may21hw.py
@@ -0,0 +1,10 @@
+students = ["sai","rahul","rohit","sita","gita","ravi","kumar"]
+
+# print rohit from the list 
+print(students[2])
+
+# print tihor from the list 
+print(students[2][::-1])
+
+#print h from rohit 
+print(students[2][2])
