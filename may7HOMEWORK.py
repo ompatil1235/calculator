282639d9c743ae79825c5a0cdc35edeb895442db
om patil <omdeven1432@gmail.com>
2026-05-22 15:18:04 +0530

    Add files via upload

---
+++ may7HOMEWORK.py
@@ -0,0 +1,12 @@
+model="macbookpro"
+price=99999.99
+realese_year=2024
+print(f"model: {model} , price: {price} , realese year: {realese_year}")
+
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+city=input("enter your city: ")
+district=input("enter your district: ")
+print(" ")
+print(f"my name is {name} and i am {age} years old. i live in {city} city and {district} district.")
+print(" ")
